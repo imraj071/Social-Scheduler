6bbd7f374efa3d9c83c1f60029fb7bac3b44ff59
imraj071 <imraj071@gmail.com>
2026-07-12 19:42:58 +0530

    Create Account Model

---
+++ server/models/account.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    platform: {type: String, enum:["twitter", "linkedin", "facebook", "instgram", "facebook_page", "linkedin_page", "instagram_business"], required: true},
+    handle: {type: String, required: true},
+    ZernioAccountId: {type: String},
+    accessToken: {type: String},
+    refreshToken: {type: String},
+    tokenExpiresAt: {type: Date},
+    status: {type: String, enum: ["connected", "disconnected"], default: "connected"},
+    avatarUrl: {type: String}
+}, {timestamps: true})
+
+export const Account = mongoose.model("Account", accountSchema);
+
