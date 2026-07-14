4b6a4d24e8ea74220869d0a6cee2601b868a5e5a
imraj071 <imraj071@gmail.com>
2026-07-14 19:27:08 +0530

    Create Post Controller functions

---
+++ server/controllers/postController.ts
@@ -0,0 +1,43 @@
+import { Response } from "express";
+import { AuthRequest } from "../middlewares/authMiddleware.js";
+
+
+//Generate post
+// POST /api/posts/generate 
+export const generatePost = async (req : AuthRequest, res: Response): Promise<void> => {
+    try {
+        
+    } catch (error:any) {
+        
+    }
+}
+
+//Get generations
+// GET /api/posts/generations
+export const getGenerations = async (req : AuthRequest, res: Response): Promise<void> => {
+    try {
+        
+    } catch (error:any) {
+        
+    }
+}
+
+//Get posts
+// GET /api/posts
+export const getPosts = async (req : AuthRequest, res: Response): Promise<void> => {
+    try {
+        
+    } catch (error:any) {
+        
+    }
+}
+
+//Schedule posts
+// POST /api/posts
+export const schedulePosts = async (req : AuthRequest, res: Response): Promise<void> => {
+    try {
+        
+    } catch (error:any) {
+        
+    }
+}
