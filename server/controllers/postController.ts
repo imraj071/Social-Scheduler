ecab9526d6c55b2fc929f1b5f49ac94a450359e5
imraj071 <imraj071@gmail.com>
2026-07-21 20:22:53 +0530

    Create Controller to generate posts

--- server/controllers/postController.ts
+++ server/controllers/postController.ts
@@ -1,14 +1,143 @@
 import { Response } from "express";
 import { AuthRequest } from "../middlewares/authMiddleware.js";
+import { GoogleGenAI } from "@google/genai";
+import axios from "axios";
+import { cloudinary } from "../config/cloudinary.js";
+import { Generation } from "../models/generation.js";
 
+//Helper to poll Leonardo.ai
+const pollLeonardoJob = async (generationId: string, apiKey: string): Promise<string> => {
+    const maxRetries = 20;
+    const delay = 5000;
+
+    for(let i=0;i<maxRetries;i++){
+        try{
+            const response = await axios.get(`https://cloud.leonardo.ai/api/rest/v1/generations/${generationId}`, {
+                headers: {
+                    accept: "application/json",
+                    authorization: `Bearer ${apiKey}`,
+                }
+            })
+            
+            const generation = response.data.generations_by_pk;
+            if(generation.status === "COMPLETE"){
+                if(generation.generated_images && generation.generated_images.length >0){
+                    return generation.generated_images[0].url
+                }
+                throw new Error("Generation complete but no images found.")
+            }
+
+            if(generation.status === "FAILED"){
+                throw new Error("Leonardo.ai generation failed")
+            }
+        
+        } catch (err: any){
+            console.log("Polling error: ", err?.response.data || err.message);
+        }
+    }
+}
 
 //Generate post
 // POST /api/posts/generate 
 export const generatePost = async (req : AuthRequest, res: Response): Promise<void> => {
     try {
+
+        const {prompt, tone, generateImage} = req.body;
+
+
+        const apiKey = process.env.GEMINI_API_KEY;
+        if(!apiKey){
+            res.status(400).json({message:"Gemini API Key is missing. Please add it to your server/.env file."})
+            return;
+        }
+
+        const ai = new GoogleGenAI({apiKey});
+
+        //Generate Text
+        const textResponse = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: `Generate a social media post based on this prompt: "${prompt}".,
+            Tone: ${tone}.
+            Include relevant hastags.
+            Format the response as JSON with "content" and "imagePrompt" fields.
+            The "imagePrompt" should be a highly desriptive prompt for an image generator that complements the post. Do not include any other text in the response.`,
+        });
+
+        let content = "";
+        let imagePrompt = prompt;
+
+        try{
+            const rawText = textResponse.text || "";
+            const jsonMatch = rawText.match(/\{[\s\S]*\}/);
+            const data = jsonMatch ? JSON.parse(jsonMatch[0]) : {content: rawText, imagePrompt: prompt};
+            content = data.content;
+            imagePrompt = data.imagePrompt;
+        } catch (error:any) {
+            content = textResponse.text || "";
+        }
+
+        let mediaUrl = "";
+        if(generateImage){
+            try {
+
+                const leonardoKey = process.env.LEONARDO_API_KEY;
+                if(leonardoKey){
+                    //Use Leonardo.ai for image generation
+                    const leoResponse = await axios.post(
+                        "https://cloud.leonardo.ai/api/rest/v2/generations",
+                        {
+                            "public": false,
+                            "model": "gpt-image-1.5",
+                            "parameters": {
+                                "quality": "LOW",
+                                "prompt": imagePrompt,
+                                "quantity": 1,
+                                "width": 1024,
+                                "height": 1024,
+                                "prompt_enhance": "OFF",
+                            } 
+                        }, {
+                                headers: {
+                                    accept: "application/json",
+                                    authorization: `Bearer ${leonardoKey}`,
+                                    "Content-Type": "application/json",
+                                }
+                            }  
+                    )
+
+                    const generationId = leoResponse.data.generate.generationId;
+                    const tempUrl = await pollLeonardoJob(generationId,leonardoKey);
+
+                    //Upload to cloudinary for persistence
+                    const uploadResult = await cloudinary.uploader.upload(tempUrl, {
+                        folder: 'ai-generations',
+                    });
+
+                    mediaUrl = uploadResult.secure_url;
+
+                    
+                }
+                
+            } catch (error: any) {
+                console.error("Image generation failed: ",error);
+            }
+        }
+
+        //Save generation to DB
+        const generation = await Generation.create({
+            user: req.user._id,
+            prompt,
+            content,
+            mediaUrl,
+            mediaType: mediaUrl ? "image" : undefined,
+            tone
+        })
+
+        res.json(generation)
+
         
     } catch (error:any) {
-        
+        res.status(500).json({message: error?.message || "Server Error"})
     }
 }
 
