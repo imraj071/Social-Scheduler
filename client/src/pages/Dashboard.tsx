3af33f36efa1b5f75f77ebf1452990b986cfa823
imraj071 <imraj071@gmail.com>
2026-08-01 00:10:47 +0530

    Replace dummyData with api data in Dashboard page

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { ActivityIcon, CheckCircleIcon, ClockIcon, SendIcon, Share2Icon, TrendingUpIcon } from "lucide-react"
 import { useEffect, useState } from "react"
-import { dummyAccountsData, dummyActivityData, dummyPostsData } from "../assets/assets"
+import api from "../api/axios"
 
 const Dashboard = () => {
 
@@ -15,7 +15,8 @@ const Dashboard = () => {
   useEffect(()=>{
     const fetchDashboardData = async()=>{
         try {
-          const [postRes,accountsRes,activityRes] = [{data: dummyPostsData}, {data: dummyAccountsData}, {data: dummyActivityData}]
+          const [postRes,accountsRes,activityRes] = await Promise.all([api.get("/api/posts"), api.get("/api/accounts"), api.get("/api/activity")])
+
           const posts = postRes.data;
           setStats({
             scheduled: posts.filter((p: any) => p.status === 'scheduled').length,
